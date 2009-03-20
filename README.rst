5697535238b0ff9b91a98900be002a49503b94dd
mpj <mpj@dneg.com>
2009-03-20 18:14:44 +0000

    Added setup explanation

--- README.rst
+++ README.rst
@@ -10,3 +10,18 @@ Status
 
 Still in planning and basic design stage.
 
+Running Testsuite
+-----------------
+
+The ``generateDS.py`` scripts referenced below is Dave Kuhlman's Python module
+available from `http://www.rexx.com/~dkuhlman/generateDS.html#download`.
+
+::
+   cd doxparsers/
+   cp /path/to/generateDS.py generateDS.py
+   ./generate.sh
+   cd ../testsuite
+   make html
+
+Then view the resulting html with your browser of choice.
+
--- doxparsers/generate.sh
+++ doxparsers/generate.sh
@@ -2,14 +2,31 @@
 
 directory=`pwd`
 
+# Make all the examples in the doxygen directory
 cd ../examples/doxygen
-
 make 
 
+# Create links to examples of the .xsd files 
+# (It doesn't matter which ones they are all the same)
 cd $directory
 
-ln -s ../examples/doxygen/class/xml/index.xsd .
-ln -s ../examples/doxygen/class/xml/compound.xsd .
+if [ ! -e index.xsd ]
+then
+	ln -s ../examples/doxygen/class/xml/index.xsd .
+fi
+
+if [ ! -e compound.xsd ]
+then
+	ln -s ../examples/doxygen/class/xml/compound.xsd .
+fi
+
+if [ ! -e generateDS.py ]
+then
+	echo "Error - Cannot find generateDS.py in this directory."
+	echo "        Please get it from:"
+	echo "        http://www.rexx.com/~dkuhlman/generateDS.html#download"
+	exit
+fi
 
 python generateDS.py --no-process-includes -o indexsuper.py -a "xsd:" index.xsd
 
