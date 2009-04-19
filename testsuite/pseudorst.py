34296268abbe50925c9c5f767cc589db1966fcb0
mpj <mpj@dneg.com>
2009-04-19 20:36:28 +0100

    Test script for viewing pseudo xml output for debugging

---
+++ testsuite/pseudorst.py
@@ -0,0 +1,18 @@
+
+
+import sys
+
+sys.path.insert(0, "../")
+
+import breathe
+import docutils
+
+from docutils.core import publish_cmdline, default_description
+
+from docutils.parsers.rst import directives
+directives.register_directive("doxygenindex", breathe.DoxygenIndexDirective)
+
+description = ('Generates pseudo-XML from standalone reStructuredText '
+               'sources (for testing purposes).  ' + default_description)
+
+publish_cmdline(description=description)
