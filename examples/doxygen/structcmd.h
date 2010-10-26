d49d79aa6b5368b6229d202a8c2e0495407dab99
Marti Bolivar <mbolivar@mit.edu>
2010-10-25 20:13:57 -0400

    example demonstrating doxygenfunction misbehavior when long description is present.

--- examples/doxygen/structcmd.h
+++ examples/doxygen/structcmd.h
@@ -25,6 +25,8 @@
 /*! \fn int open(const char *pathname,int flags)
     \brief Opens a file descriptor.
 
+    Detailed description.
+    
     \param pathname The name of the descriptor.
     \param flags Opening flags.
 */
