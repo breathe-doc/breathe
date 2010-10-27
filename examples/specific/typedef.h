148091d3e154b9bdcd76e0c849f1aa311caf7f46
Michael Jones <mjones@wetafx.co.nz>
2010-10-28 08:30:33 +1300

    Adding more typedef examples
    Leaving further work until the parsing is refactored.
    
    Ideally, I'd rather see:
    
    	typedef void (*func)(void)
    
    than:
    
    	typedef void (* func)(void)
    
    But it is too much trouble to navigate the nodes and figure out if we're
    a function pointer typedef in hindsight. Hoping it'll be easier at
    parsing time if we get the chance.

---
+++ examples/specific/typedef.h
@@ -0,0 +1,15 @@
+
+class TypeDefTest {
+};
+
+/* A dummy typedef */
+typedef TypeDefTest (*TypeDefTestFuncPtr)(void);
+
+typedef void* (*voidFuncPtr)(float, int);
+
+typedef void* voidPointer;
+
+typedef float* floatPointer;
+
+typedef float floatingPointNumber;
+
