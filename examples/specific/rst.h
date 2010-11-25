b88a2056057da724d992b0cc26ba9dd5c12a9618
Michael Jones <mjones@wetafx.co.nz>
2010-11-26 08:34:21 +1300

    Handle non-embed:rst verbatim tags as literal blocks
    Gives the standard Sphinx literal block with preformatted text between.
    Seems like the most reasonable way.
    
    Future work might support other embed:* formats which could be given
    to Sphinx as language hints for highlighting.

--- examples/specific/rst.h
+++ examples/specific/rst.h
@@ -16,6 +16,13 @@ public:
        This restructured text has been handled correctly.
     \endrst
 
+    \verbatim
+    This is just a standard verbatim block with code:
+
+        child = 0;
+        while( child = parent->IterateChildren( child ) )
+    \endverbatim
+
     */
     virtual void function() const = 0;
 
