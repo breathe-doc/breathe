047c8530c806ed5d9c24e3cf0edfa0aadaa6566b
Michael Jones <m.pricejones@gmail.com>
2012-11-04 17:51:01 +1300

    Reformatting to keep with generally code style

--- breathe/renderer/rst/doxygen/filter.py
+++ breathe/renderer/rst/doxygen/filter.py
@@ -262,21 +262,28 @@ class FilterFactory(object):
 
     def create_members_filter(self, options):
 
-
         try:
             section = options["sections"]
         except KeyError:
             section = ""
 
         if not section.strip():
-            sectionFilter = GlobFilter(KindAccessor(Child()), self.globber_factory.create("public*"))
+            section_filter = GlobFilter(KindAccessor(Child()), self.globber_factory.create("public*"))
         else:
             sections = set([x.strip() for x in section.split(",")])
 
-            sectionFilter = GlobFilter(KindAccessor(Child()), self.globber_factory.create(  sections.pop()  ))
+            section_filter = GlobFilter(
+                    KindAccessor(Child()),
+                    self.globber_factory.create(sections.pop())
+                    )
             while len(sections) > 0:
-                sectionFilter = OrFilter( sectionFilter , GlobFilter(KindAccessor(Child()), self.globber_factory.create(  sections.pop()  )) )
-
+                section_filter = OrFilter(
+                        section_filter,
+                        GlobFilter(
+                            KindAccessor(Child()),
+                            self.globber_factory.create(sections.pop())
+                            )
+                        )
 
         try:
             text = options["members"]
@@ -290,7 +297,7 @@ class FilterFactory(object):
             return OrFilter(
                     NotFilter(NameFilter(NodeTypeAccessor(Child()), ["sectiondef"])),
                     OrFilter(
-                        sectionFilter,
+                        section_filter,
                         NameFilter(KindAccessor(Child()), ["user-defined"])
                         )
                     )
