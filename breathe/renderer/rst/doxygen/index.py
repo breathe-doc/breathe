breathe-doc/breathe

commit 257473e257a91764d0f29f181f2945bd2f46d72e

Support template parameters for classes and functions
We introduce the TemplateParamListRenderer which handles iterating over
its parameters and formatting the output.

We then add code to create and call this renderer for classes and
standalone functions.

We format the output using a line_block so that the template declaration
and class/function names can appear on consecutive lines without a large
paragraph break between them.

We also find that unfortunately the doxygen xml for integer template
parameters contains the parameter name twice as both the declname and
defname so we have to introduce a new check to the parameter code to
test if we want both of these output or just one and we make sure it is
properly prepped for the template case.

Finally, we stop the LinkedTextTypeSubRenderer appending spaces to the
end of its results and have the renderer above it control the spacing as
it more appropriate. This is necessary other in the template case we
would have a space between the template parameter name and the following
comma.

Note: We've found that the first entry in the "line" element gets
interpreted literally so non-text nodes get expanded out to their text
equivalent, eg. <emphasis>int</emphasis>. To avoid this we add some zero
length strings in the first element. Not sure what is causing this.

breathe/renderer/rst/doxygen/index.py
@@ -37,24 +37,49 @@ class CompoundTypeSubRenderer(Renderer):
 
         domain_id = self.create_domain_id()
 
-        # Set up the title and a reference for it (refid)
-        kind = self.node_factory.emphasis(text=self.data_object.kind)
-        name = self.node_factory.strong(text=self.data_object.name)
-        nodelist.append(
-                self.node_factory.paragraph(
-                    "",
-                    "",
-                    kind,
-                    self.node_factory.Text(" "),
-                    name,
-                    ids=[domain_id, refid]
-                    )
-            )
 
         # Read in the corresponding xml file and process
         file_data = self.compound_parser.parse(self.data_object.refid)
-        data_renderer = self.renderer_factory.create_renderer(self.data_object, file_data)
 
+        lines = []
+
+        # Check if there is template information and format it as desired
+        if file_data.compounddef.templateparamlist:
+            renderer = self.renderer_factory.create_renderer(
+                    file_data.compounddef,
+                    file_data.compounddef.templateparamlist
+                    )
+            template = [
+                    self.node_factory.Text("template < ")
+                ]
+            template.extend(renderer.render())
+            template.append(self.node_factory.Text(" >"))
+            lines.append(self.node_factory.line("",*template))
+
+        # Set up the title and a reference for it (refid)
+        kind = self.node_factory.emphasis(text=self.data_object.kind)
+        name = self.node_factory.strong(text=self.data_object.name)
+
+        # Add blank string at the start otherwise for some reason it renders
+        # the emphasis tags around the kind in plain text
+        lines.append(
+                self.node_factory.line(
+                    "", 
+                    self.node_factory.Text(""),
+                    kind,
+                    self.node_factory.Text(" "),
+                    name
+                    )
+                )
+
+        nodelist.append(
+                self.node_factory.line_block(
+                    "",
+                    *lines
+                    )
+                )
+
+        data_renderer = self.renderer_factory.create_renderer(self.data_object, file_data)
         nodelist.extend(data_renderer.render())
 
         return nodelist

breathe/renderer/rst/doxygen/__init__.py
@@ -177,6 +177,12 @@ class DoxygenToRstRendererFactory(object):
             elif data_object.kind == "define":
                 Renderer = compoundrenderer.DefineMemberDefTypeSubRenderer
 
+        if node_type == "param":
+            return Renderer(
+                    parent_data_object.node_type != "templateparamlist", 
+                    *common_args
+                    )
+
 
         if node_type == "docsimplesect":
             if data_object.kind == "par":
@@ -252,6 +258,7 @@ class DoxygenToRstRendererFactoryCreator(object):
             "docsect1" : compoundrenderer.DocSect1TypeSubRenderer,
             "docsimplesect" : compoundrenderer.DocSimpleSectTypeSubRenderer,
             "doctitle" : compoundrenderer.DocTitleTypeSubRenderer,
+            "templateparamlist" : compoundrenderer.TemplateParamListRenderer,
             "ref" : CreateRefTypeSubRenderer(self.parser_factory),
             "verbatim" : compoundrenderer.VerbatimTypeSubRenderer,
             "mixedcontainer" : compoundrenderer.MixedContainerRenderer,

breathe/renderer/rst/doxygen/compound.py
@@ -242,6 +242,31 @@ class FuncMemberDefTypeSubRenderer(MemberDefTypeSubRenderer):
 
     def title(self):
 
+        lines = []
+
+        # Handle any template information
+        if self.data_object.templateparamlist:
+            renderer = self.renderer_factory.create_renderer(
+                    self.data_object,
+                    self.data_object.templateparamlist
+                    )
+            template = [
+                    self.node_factory.Text("template < ")
+                ]
+            template.extend(renderer.render())
+            template.append(self.node_factory.Text(" >"))
+
+            # Add blank string at the start otherwise for some reason it renders
+            # the emphasis tags around the kind in plain text (same below)
+            lines.append(
+                    self.node_factory.line(
+                        "",
+                        self.node_factory.Text(""),
+                        *template
+                        )
+                    )
+
+        # Get the function type and name
         args = MemberDefTypeSubRenderer.title(self)
 
         # Get the function arguments
@@ -252,7 +277,21 @@ class FuncMemberDefTypeSubRenderer(MemberDefTypeSubRenderer):
             args.extend(renderer.render())
         args.append(self.node_factory.Text(")"))
 
-        return args
+        lines.append(
+                self.node_factory.line(
+                    "",
+                    self.node_factory.Text(""),
+                    *args
+                    )
+                )
+
+        # Setup the line block with gathered informationo
+        block = self.node_factory.line_block(
+                "",
+                *lines
+                )
+
+        return [block]
 
 
 class DefineMemberDefTypeSubRenderer(MemberDefTypeSubRenderer):
@@ -371,16 +410,26 @@ class LinkedTextTypeSubRenderer(Renderer):
         nodelist = []
 
         # Recursively process where possible
-        for i in self.data_object.content_:
-            renderer = self.renderer_factory.create_renderer(self.data_object, i)
+        for i, entry in enumerate(self.data_object.content_):
+            if i:
+                nodelist.append(self.node_factory.Text(" "))
+            renderer = self.renderer_factory.create_renderer(self.data_object, entry)
             nodelist.extend(renderer.render())
-            nodelist.append(self.node_factory.Text(" "))
-
 
         return nodelist
 
 
 class ParamTypeSubRenderer(Renderer):
+
+    def __init__(
+            self,
+            output_defname,
+            *args
+            ):
+
+        Renderer.__init__( self, *args )
+
+        self.output_defname = output_defname
 
     def render(self):
 
@@ -393,9 +442,11 @@ class ParamTypeSubRenderer(Renderer):
 
         # Parameter name
         if self.data_object.declname:
+            nodelist.append(self.node_factory.Text(" "))
             nodelist.append(self.node_factory.Text(self.data_object.declname))
 
-        if self.data_object.defname:
+        if self.output_defname and self.data_object.defname:
+            nodelist.append(self.node_factory.Text(" "))
             nodelist.append(self.node_factory.Text(self.data_object.defname))
 
         # Default value
@@ -605,6 +656,20 @@ class DocTitleTypeSubRenderer(Renderer):
 
         for item in self.data_object.content_:
             renderer = self.renderer_factory.create_renderer(self.data_object, item)
+            nodelist.extend(renderer.render())
+
+        return nodelist
+
+class TemplateParamListRenderer(Renderer):
+
+    def render(self):
+
+        nodelist = []
+
+        for i, param in enumerate(self.data_object.param):
+            if i:
+                nodelist.append(self.node_factory.Text(", "))
+            renderer = self.renderer_factory.create_renderer(self.data_object, param)
             nodelist.extend(renderer.render())
 
         return nodelist

examples/specific/template_class_non_type.h
@@ -18,7 +18,7 @@ public:
    * @param m1 first argument
    * @param m2 second argument
    */
-  anothertemplateclass(T const & m1, U const & m2) :
+  anothertemplateclass(T const & m1, U const & m2) : 
     member1(m1), member2(m2) {}
     
   /**

commit 5c767aaa58a35e3ac97795d7988aa28935aa5546

Output domain & ref ids for linking
Otherwise links to classes and other index level items fail.

The regression was introduced in:

    257473e257a91764d0f29f181f2945bd2f46d72e

breathe/renderer/rst/doxygen/index.py
@@ -68,7 +68,8 @@ class CompoundTypeSubRenderer(Renderer):
                     self.node_factory.Text(""),
                     kind,
                     self.node_factory.Text(" "),
-                    name
+                    name,
+                    ids=[domain_id, refid]
                     )
                 )
 