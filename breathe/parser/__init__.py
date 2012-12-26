fb8045969e4ab4bea06b3e5f9a6dd93828e2e79c
Jim Naslund <jnaslund@gmail.com>
2012-12-26 14:35:08 -0500

    Scan doxygen xml to determine docnames to rebuild
    This means changes to doxygen xml files will caused rst documents that
    use those files to be rebuilt

--- breathe/parser/__init__.py
+++ breathe/parser/__init__.py
@@ -8,17 +8,29 @@ class ParserError(Exception):
 
 class Parser(object):
 
-    def __init__(self, cache, path_handler):
+    def __init__(self, cache, path_handler, app):
 
         self.cache = cache
         self.path_handler = path_handler
+        self.app = app
 
+    def store_file_state(self, filename):
+        if not hasattr(self.app.env, 'file_state'):
+            self.app.env.file_state = {}
+        newmtime = os.path.getmtime(filename)
+        mtime, docnames = self.app.env.file_state.setdefault(
+            filename, (newmtime, set()))
+        self.app.env.file_state[filename] = (newmtime, docnames)
+        docnames.add(self.app.env.docname)
 
 class DoxygenIndexParser(Parser):
 
-    def parse(self, project_info):
+    def __init__(self, cache, path_handler, app):
+        Parser.__init__(self, cache, path_handler, app)
 
+    def parse(self, project_info):
         filename = self.path_handler.join(project_info.path(), "index.xml")
+        self.store_file_state(filename)
 
         try: 
             # Try to get from our cache
@@ -35,14 +47,15 @@ class DoxygenIndexParser(Parser):
 
 class DoxygenCompoundParser(Parser):
 
-    def __init__(self, cache, path_handler, project_info):
-        Parser.__init__(self, cache, path_handler)
+    def __init__(self, cache, path_handler, project_info, app):
+        Parser.__init__(self, cache, path_handler, app)
 
         self.project_info = project_info
 
     def parse(self, refid):
 
         filename = self.path_handler.join(self.project_info.path(), "%s.xml" % refid)
+        self.store_file_state(filename)
 
         try: 
             # Try to get from our cache
@@ -66,18 +79,20 @@ class CacheFactory(object):
 
 class DoxygenParserFactory(object):
 
-    def __init__(self, cache, path_handler):
+    def __init__(self, cache, path_handler, app):
 
         self.cache = cache
         self.path_handler = path_handler
+        self.app = app
 
     def create_index_parser(self):
 
-        return DoxygenIndexParser(self.cache, self.path_handler)
+        return DoxygenIndexParser(self.cache, self.path_handler, self.app)
 
     def create_compound_parser(self, project_info):
 
-        return DoxygenCompoundParser(self.cache, self.path_handler, project_info)
+        return DoxygenCompoundParser(self.cache, self.path_handler,
+                                     project_info, self.app)
 
 
 
