4ad1d265aa032f55555788aa9fb8724e57d57bbd
Michael Jones <m.pricejones@gmail.com>
2013-06-28 20:29:11 +1200

    Tweak the asterisk handling logic and docs
    We change the documentation to output a representation of the bad data
    rather than feeding it the bad data and letting it give us a warning as
    we're keen to keep warnings to a minimum.
    
    The test case in the documentation was failing as the first character of
    the line wasn't "*" so we tweak the logic to replace the first occurance
    of * instead of assuming it is the first character.

--- breathe/renderer/rst/doxygen/__init__.py
+++ breathe/renderer/rst/doxygen/__init__.py
@@ -14,7 +14,7 @@ class RstContentCreator(object):
 
     def __call__(self, text):
 
-        # Remove the first line
+        # Remove the first line which is "embed:rst[:leading-asterisk]"
         text = "\n".join(text.split(u"\n")[1:])
 
         # Remove starting whitespace
