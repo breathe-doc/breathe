88cf0f33d44c3973ad1467189e4a633a82f0904b
Michael Jones <m.pricejones@gmail.com>
2013-10-08 17:22:51 +1300

    Add program listing example
    As reported by jo3w4rd on #65 on github.

---
+++ examples/specific/programlisting.h
@@ -0,0 +1,11 @@
+/**
+ * The center of the InteractionBox in device coordinates (millimeters). This point
+ * is equidistant from all sides of the box.
+ *
+ * \include programlistinginclude.txt
+ *
+ * @returns The InteractionBox center in device coordinates.
+ * @since 1.0
+ */
+LEAP_EXPORT Vector center() const;
+
