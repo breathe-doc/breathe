88f14c1f47c67de0129a92daa280150294059da9
Michael Jones <m.pricejones@gmail.com>
2014-04-04 03:56:13 +1300

    Examples to test the autodoxygenindex behaviour
    It was providing none intuitive results for certain configurations.

---
+++ examples/specific/auto_class.h
@@ -0,0 +1,12 @@
+
+//! \brief class outside of namespace
+class AutoClassTest {
+
+    //! \brief non-namespaced class function
+    void member() {};
+
+    //! \brief non-namespaced class other function
+    void anotherMember() {};
+};
+
+
