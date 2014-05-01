495f5ffeda1f167a51efe876aa67020e22e89855
Michael Jones <m.pricejones@gmail.com>
2014-05-01 12:55:20 +0100

    Change autofile approach to predeclared file names
    Prior to this commit we were attempting to implement the
    auto-functionality by having placeholder custom docutils nodes which
    stored the required code source files for use with doxygen. Then the
    plan was to do a pass over all the nodes, figure out all the files we
    needed to generate doxygen xml for, generate it and then go through and
    replace all the placeholder nodes with actual doxygen documentation.
    
    Unfortunately, Sphinx/docutils doesn't work like that. We can insert the
    nodes but then all transformations to the first document and its nodes
    have to be done before processing of the second document has begun. So
    we can't collect information from all the documents and return to the
    first document to transform/process it with our new found knowledge.
    
    So, as we can't automatically collect the list of files to be
    doxygen-processed from the rst files, we have to ask the users to
    pre-declare them in the config.py so that we can do everything we need
    before we start to process the documents.
    
    This simplies things quite a bit and we can strip out all the custom
    node and transform code we had and instead preprocess all the declared
    files in the "builder-init" hook that Sphinx provides us.
    
    When it comes to the directives, it is then just a case of figuring out
    if we're a normal directive in which case we get our project_info from
    the config or if we're an autodirective in which case we project_info is
    determined by the preprocessing step.
    
    I imagine the code could be a little better at this stage. In
    particular, the management of the doxygen xml generation. We should be
    storing some timestamps and file associations to have a better idea of
    when to regenerate the xml.

--- breathe/directive/index.py
+++ breathe/directive/index.py
@@ -1,6 +1,6 @@
 
 from breathe.renderer.rst.doxygen import format_parser_error
-from breathe.directive.base import BaseDirective, BaseNodeHandler
+from breathe.directive.base import BaseDirective
 from breathe.project import ProjectError
 from breathe.parser import ParserError, FileIOError
 from breathe.nodes import DoxygenNode, DoxygenAutoNode
@@ -9,18 +9,12 @@ from docutils.parsers.rst.directives import unchanged_required, flag
 from docutils import nodes
 
 
-class IndexNodeHandler(BaseNodeHandler):
-    """
-    Replaces a DoxygenNode with the rendered contents of the doxygen xml's index.xml file
+class BaseIndexDirective(BaseDirective):
 
-    This used to be carried out in the doxygenindex directive implementation but we have this level
-    of indirection to support the autodoxygenindex directive and share the code.
-    """
-
-    def render(self):
+    def handle_contents(self, project_info):
 
         try:
-            finder = self.factories.finder_factory.create_finder(self.project_info)
+            finder = self.finder_factory.create_finder(project_info)
         except ParserError, e:
             return format_parser_error(self.name, e.error, e.filename, self.state, self.lineno, True)
         except FileIOError, e:
@@ -28,11 +22,11 @@ class IndexNodeHandler(BaseNodeHandler):
 
         data_object = finder.root()
 
-        target_handler = self.factories.target_handler_factory.create_target_handler(self.options, self.project_info, self.state.document)
-        filter_ = self.factories.filter_factory.create_index_filter(self.options)
+        target_handler = self.target_handler_factory.create_target_handler(self.options, project_info, self.state.document)
+        filter_ = self.filter_factory.create_index_filter(self.options)
 
-        renderer_factory_creator = self.factories.renderer_factory_creator_constructor.create_factory_creator(
-                self.project_info,
+        renderer_factory_creator = self.renderer_factory_creator_constructor.create_factory_creator(
+                project_info,
                 self.state.document,
                 self.options,
                 target_handler
@@ -44,7 +38,7 @@ class IndexNodeHandler(BaseNodeHandler):
                 filter_,
                 target_handler,
                 )
-        object_renderer = renderer_factory.create_renderer(self.factories.root_data_object, data_object)
+        object_renderer = renderer_factory.create_renderer(self.root_data_object, data_object)
 
         try:
             node_list = object_renderer.render()
@@ -56,7 +50,7 @@ class IndexNodeHandler(BaseNodeHandler):
         return node_list
 
 
-class DoxygenIndexDirective(BaseDirective):
+class DoxygenIndexDirective(BaseIndexDirective):
 
     required_arguments = 0
     optional_arguments = 2
@@ -77,26 +71,15 @@ class DoxygenIndexDirective(BaseDirective):
             return [nodes.warning("", nodes.paragraph("", "", nodes.Text(warning))),
                     self.state.document.reporter.warning(warning, line=self.lineno)]
 
-        handler = IndexNodeHandler(
-                "doxygenindex",
-                None,               # No data required for doxygenindex
-                project_info,
-                self.options,
-                self.state,
-                self.lineno,
-                self
-                )
-
-        return [DoxygenNode(handler)]
+        return self.handle_contents(project_info)
 
 
-class AutoDoxygenIndexDirective(BaseDirective):
+class AutoDoxygenIndexDirective(BaseIndexDirective):
 
-    required_arguments = 1
+    required_arguments = 0
     final_argument_whitespace = True
     option_spec = {
-            "source-path": unchanged_required,
-            "source": unchanged_required,
+            "project": unchanged_required,
             "outline": flag,
             "no-link": flag,
             }
@@ -104,24 +87,13 @@ class AutoDoxygenIndexDirective(BaseDirective):
 
     def run(self):
 
-        files = self.arguments[0].split()
-
         try:
-            project_info = self.project_info_factory.create_auto_project_info(self.options)
+            project_info = self.project_info_factory.retrieve_project_info_for_auto(self.options)
         except ProjectError, e:
             warning = 'autodoxygenindex: %s' % e
             return [nodes.warning("", nodes.paragraph("", "", nodes.Text(warning))),
                     self.state.document.reporter.warning(warning, line=self.lineno)]
 
-        node = DoxygenAutoNode(
-            "autodoxygenindex",
-            None,                   # No data required for doxygenindex
-            project_info,
-            files,
-            self.options,
-            self,
-            self.state,
-            self.lineno
-            )
+        return self.handle_contents(project_info)
 
-        return [node]
+
