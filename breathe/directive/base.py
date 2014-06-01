8ff589fe8f234890d1e1c900cbb03175d66c784b
Michael Jones <m.pricejones@gmail.com>
2014-06-01 21:19:30 +0100

    Flake8 fixes and reformatting

--- breathe/directive/base.py
+++ breathe/directive/base.py
@@ -8,19 +8,12 @@ from docutils.parsers import rst
 from docutils.parsers.rst.directives import unchanged_required, flag
 from docutils import nodes
 
+
 class BaseDirective(rst.Directive):
 
-    def __init__(
-            self,
-            root_data_object,
-            renderer_factory_creator_constructor,
-            finder_factory,
-            matcher_factory,
-            project_info_factory,
-            filter_factory,
-            target_handler_factory,
-            *args
-            ):
+    def __init__(self, root_data_object, renderer_factory_creator_constructor, finder_factory,
+                 matcher_factory, project_info_factory, filter_factory, target_handler_factory,
+                 *args):
         rst.Directive.__init__(self, *args)
 
         self.root_data_object = root_data_object
@@ -35,22 +28,23 @@ class BaseDirective(rst.Directive):
         "Standard render process used by subclasses"
 
         renderer_factory_creator = self.renderer_factory_creator_constructor.create_factory_creator(
-                project_info,
-                self.state.document,
-                self.options,
-                target_handler
-                )
+            project_info,
+            self.state.document,
+            self.options,
+            target_handler
+            )
 
         try:
             renderer_factory = renderer_factory_creator.create_factory(
-                    data_object,
-                    self.state,
-                    self.state.document,
-                    filter_,
-                    target_handler,
-                    )
+                data_object,
+                self.state,
+                self.state.document,
+                filter_,
+                target_handler,
+                )
         except ParserError, e:
-            return format_parser_error("doxygenclass", e.error, e.filename, self.state, self.lineno, True)
+            return format_parser_error("doxygenclass", e.error, e.filename, self.state,
+                                       self.lineno, True)
         except FileIOError, e:
             return format_parser_error("doxygenclass", e.error, e.filename, self.state, self.lineno)
 
@@ -59,16 +53,17 @@ class BaseDirective(rst.Directive):
 
         return node_list
 
+
 class DoxygenBaseDirective(BaseDirective):
 
     required_arguments = 1
     optional_arguments = 1
     option_spec = {
-            "path": unchanged_required,
-            "project": unchanged_required,
-            "outline": flag,
-            "no-link": flag,
-            }
+        "path": unchanged_required,
+        "project": unchanged_required,
+        "outline": flag,
+        "no-link": flag,
+        }
     has_content = False
 
     def run(self):
@@ -93,12 +88,18 @@ class DoxygenBaseDirective(BaseDirective):
             data_object = finder.find_one(matcher_stack)
         except NoMatchesError, e:
             display_name = "%s::%s" % (namespace, name) if namespace else name
-            warning = ('doxygen%s: Cannot find %s "%s" in doxygen xml output for project "%s" from directory: %s'
-                    % (self.kind, self.kind, display_name, project_info.name(), project_info.project_path()))
+            warning = (
+                'doxygen%s: Cannot find %s "%s" in doxygen xml output for project "%s" from '
+                'directory: %s' % (
+                    self.kind, self.kind, display_name, project_info.name(),
+                    project_info.project_path()
+                    )
+                )
             return [nodes.warning("", nodes.paragraph("", "", nodes.Text(warning))),
                     self.state.document.reporter.warning(warning, line=self.lineno)]
 
-        target_handler = self.target_handler_factory.create_target_handler(self.options, project_info, self.state.document)
+        target_handler = self.target_handler_factory.create_target_handler(
+            self.options, project_info, self.state.document)
         filter_ = self.filter_factory.create_outline_filter(self.options)
 
         return self.render(data_object, project_info, filter_, target_handler)
