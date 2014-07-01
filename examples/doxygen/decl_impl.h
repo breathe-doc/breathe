2a6969170923480dfee58fa216ea2d5b49a6da50
Per Rosengren <per.rosengren@neonode.com>
2014-07-01 15:39:14 +0200

    Breaking test: Unable to resolve multiple matches for function
    When a function is both declared and implemented, doxygenfunction fails with the message:
    
    Warning
    doxygenfunction: Unable to resolve multiple matches for function “open_di” with arguments () in doxygen xml output for project “decl_impl” from directory: ../../examples/doxygen/decl_impl/xml/
    
    View example in:
    function.html#separated-declaration-implementation-example

---
+++ examples/doxygen/decl_impl.h
@@ -0,0 +1,9 @@
+/*! \fn int open_di(const char *pathname,int flags)
+    \brief Opens a file descriptor.
+
+    Detailed description.
+    
+    \param pathname The name of the descriptor.
+    \param flags Opening flags.
+*/
+int open_di(const char *,int);
