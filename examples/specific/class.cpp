ea0fa9107d89d00871d17cf13074317970f838f5
Michael Jones <m.pricejones@gmail.com>
2014-07-01 21:57:28 +0100

    Move decl_impl example to specific folder
    And extend the class example to have an impl file as well for
    testing.

---
+++ examples/specific/class.cpp
@@ -0,0 +1,12 @@
+#include "class.h"
+
+/*! More documentation in the impl file */
+void ClassTest::function()
+{
+}
+
+/*! More documentation in the impl file */
+void ClassTest::anotherFunction()
+{
+}
+
