46cd71213fb604caa22fdba5a810a5089e35a933
Michael Jones <m.pricejones@gmail.com>
2014-09-28 11:07:16 +0100

    Provide example with inline urls
    As they don't seem to be showing up in the Breathe output at the moment.

---
+++ examples/specific/links.h
@@ -0,0 +1,12 @@
+
+/*! \brief first struct inside of namespace
+
+  This is a longer description with a link to a webpage 
+  in the text http://www.github.com in order to test out Breathe's
+  handling of links.
+
+ */
+class Test {};
+
+
+
