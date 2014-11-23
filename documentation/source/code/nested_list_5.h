9071c9c214d946dabe5a1117081670ed437bb3af
Michael Jones <m.pricejones@gmail.com>
2014-11-23 13:52:09 +0000

    Remove deeply nested lists for pdf output
    As latex has a limit on the depth of nesting sections, though I'm not
    sure why, so we can't demo a lot of nested lists without losing the
    ability to build the pdf output.

--- documentation/source/code/nested_list_5.h
+++ documentation/source/code/nested_list_5.h
@@ -9,11 +9,6 @@
  *        -# mouse move event
  *            1. swipe event
  *              -# swipe left
- *                  1. swipe left residual (it's nonsense)
- *                  2. more nonsense
- *                  3. even more nonsense
- *                      -# deep even more nonsense
- *                  4. even even more nonsense
  *              -# swipe right
  *            2. circle event
  *            3. wave event
