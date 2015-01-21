74048c8e84e031acbc3a8d2168941a57717061e6
Victor Zverovich <victor.zverovich@gmail.com>
2015-01-20 16:07:13 -0800

    Add examples of links to typedefs

--- examples/specific/typedef.h
+++ examples/specific/typedef.h
@@ -13,3 +13,14 @@ typedef float* floatPointer;
 
 typedef float floatingPointNumber;
 
+typedef int TestTypedef;
+
+namespace testnamespace {
+  typedef char *AnotherTypedef;
+}
+
+class TestClass {
+ public:
+  /** A typedef defined in a class. */
+  typedef void *MemberTypedef;
+};
