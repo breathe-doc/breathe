cd33bb359d696062f9c50fa4713531855015bd18
Victor Zverovich <victor.zverovich@gmail.com>
2015-02-13 10:20:32 -0800

    Fix rendering of inner classes
    Pass fully-qualified name to domain directive for correct target generation,
    but filter out outer class names.

--- breathe/renderer/rst/doxygen/index.py
+++ breathe/renderer/rst/doxygen/index.py
@@ -34,12 +34,42 @@ class CompoundRenderer(Renderer):
         refid = "%s%s" % (self.project_info.name(), self.data_object.refid)
         return self.target_handler.create_target(refid)
 
+    def get_fully_qualified_name(self):
+
+        names = []
+        node_stack = self.context.node_stack
+        if node_stack[0].node_type == 'enumvalue':
+            names.append(node_stack[0].name)
+            # Skip the name of the containing enum because it is not a part of the fully qualified name.
+            node_stack = node_stack[2:]
+
+        for node in node_stack:
+            if node.node_type == 'ref':
+                return node.valueOf_
+            if (node.node_type == 'compound' and node.kind not in ['file', 'namespace']) or \
+                node.node_type == 'memberdef':
+                # We skip the 'file' entries because the file name doesn't form part of the
+                # qualified name for the identifier. We skip the 'namespace' entries because if we
+                # find an object through the namespace 'compound' entry in the index.xml then we'll
+                # also have the 'compounddef' entry in our node stack and we'll get it from that. We
+                # need the 'compounddef' entry because if we find the object through the 'file'
+                # entry in the index.xml file then we need to get the namespace name from somewhere
+                names.insert(0, node.name)
+            if (node.node_type == 'compounddef' and node.kind == 'namespace'):
+                # Nested namespaces include there parent namespace in there compoundname. ie,
+                # compoundname is 'foo::bar' instead of just 'bar' for namespace 'bar' nested in
+                # namespace 'foo'. But our node_stack includes 'foo' so we only want 'bar' at
+                # this point.
+                names.insert(0, node.compoundname.split('::')[-1])
+
+        return '::'.join(names)
+
     def render_signature(self, file_data, doxygen_target):
         # Defer to domains specific directive.
         name, kind = self.get_node_info(file_data)
         domain_directive = self.renderer_factory.domain_directive_factory.create(
             self.context.domain, [kind] + self.context.directive_args[1:])
-        domain_directive.arguments = [name]
+        domain_directive.arguments = [self.get_fully_qualified_name()]
 
         # Translate Breathe's no-link option into the standard noindex option.
         if 'no-link' in self.context.directive_args[2]:
@@ -51,6 +81,11 @@ class CompoundRenderer(Renderer):
         # The cpp domain in Sphinx doesn't support structs at the moment, so change the text from "class "
         # to the correct kind which can be "class " or "struct ".
         signode[0] = self.node_factory.desc_annotation(kind + ' ', kind + ' ')
+
+        # Filter out outer class names if we are rendering an inner class as a part of its outer class content.
+        names = self.context.directive_args[1]
+        if len(names) > 0 and names[0] != name:
+            signode.children = [n for n in signode.children if not n.tagname == 'desc_addname']
 
         # Check if there is template information and format it as desired
         template_signode = self.create_template_node(file_data.compounddef)
