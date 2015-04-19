7d8102c4ab0945f3339b04cf71f4cc83d651b14d
Michael Jones <m.pricejones@gmail.com>
2015-04-19 10:54:14 +0100

    Add more parameter arrangements to the test
    We don't need to be too careful about the formatting of the output
    because the strings are being handed to Sphinx which processes them
    again so they only need to be parseable not 100% display perfect.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,20 +9,46 @@ class TestUtils(TestCase):
 
     def test_param_decl(self):
 
-        # From declaration:
-        #    int f(int (*p)[3]);
-        # in: examples/specific/parameters.h
+        # From xml from: examples/specific/parameters.h
         xml = """
+        <memberdef>
+        <param>
+          <type>int</type>
+          <declname>a</declname>
+        </param>
+        <param>
+          <type>float</type>
+          <declname>b</declname>
+        </param>
+        <param>
+          <type>int *</type>
+          <declname>c</declname>
+        </param>
         <param>
           <type>int(*)</type>
           <declname>p</declname>
           <array>[3]</array>
         </param>
+        <param>
+          <type><ref refid="class_my_class" kindref="compound">MyClass</ref></type>
+          <declname>a</declname>
+        </param>
+        <param>
+          <type><ref refid="class_my_class" kindref="compound">MyClass</ref> *</type>
+          <declname>b</declname>
+        </param>
+        </memberdef>
         """
 
         doc = minidom.parseString(xml)
 
         memberdef = memberdefType.factory()
-        memberdef.buildChildren(doc.documentElement, 'param')
+        for child in doc.documentElement.childNodes:
+            memberdef.buildChildren(child, 'param')
 
-        self.assertEqual(get_param_decl(memberdef.param[0]), 'int(*p)[3]')
+        self.assertEqual(get_param_decl(memberdef.param[0]), 'int a')
+        self.assertEqual(get_param_decl(memberdef.param[1]), 'float b')
+        self.assertEqual(get_param_decl(memberdef.param[2]), 'int * c')
+        self.assertEqual(get_param_decl(memberdef.param[3]), 'int(*p)[3]')
+        self.assertEqual(get_param_decl(memberdef.param[4]), 'MyClass a')
+        self.assertEqual(get_param_decl(memberdef.param[5]), 'MyClass  * b')
