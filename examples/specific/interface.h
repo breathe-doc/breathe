71d1217dfce281a6c35c1c30690c5be04c3bdebb
Andy Neebel <andy@andne.net>
2017-01-24 20:55:16 -0600

    Add interface example to doxygen projects

---
+++ examples/specific/interface.h
@@ -0,0 +1,11 @@
+/* A dummy interface, which is really just a specially treated class in C++ */
+
+class InterfaceClass
+{
+};
+
+/*! \interface InterfaceClass interface.h "inc/interface.h"
+ *  \brief This is a test interface class.
+ *
+ * Some details about the InterfaceClass interface
+ */
