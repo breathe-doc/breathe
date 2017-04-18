e0b05aa2dcbca8d567242d581dce4118398a039a
Johan Mabille <johan.mabille@gmail.com>
2017-04-18 10:58:55 +0200

    headerfile command example

---
+++ examples/specific/headerfile.h
@@ -0,0 +1,29 @@
+
+/**
+ * \class ClassTest
+ * \headerfile hearderfile.h "specific/headerfile.h"
+ * \brief Class showing the header where it is defined
+ */
+class ClassTest
+{
+
+public:
+
+    //! \brief Constructor
+    ClassTest();
+};
+
+
+/**
+ * \class ClassTest2 headerfile.h "specific/headerfile.h"
+ * \brief Another way to show the header file
+ */
+class ClassTest2
+{
+
+public:
+
+    //! \brief Constructor
+    ClassTest2();
+}
+
