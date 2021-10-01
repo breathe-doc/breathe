b51ad6f159e0017506e23b15e58c1fecd634604a
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-10-01 10:22:26 +0200

    renderer: use retval type for the retval fields
    This change makes @retval fields to render as a separate list,
    independent of the @return/s entries.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- breathe/renderer/sphinxrenderer.py
+++ breathe/renderer/sphinxrenderer.py
@@ -896,7 +896,7 @@ class SphinxRenderer:
             fieldLists = [fieldList]
 
         # collapse retvals into a single return field
-        if len(fieldLists) != 0:
+        if len(fieldLists) != 0 and sphinx.version_info[0:2] < (4, 3):
             others: nodes.field = []
             retvals: nodes.field = []
             for f in fieldLists[0]:
@@ -2211,7 +2211,8 @@ class SphinxRenderer:
             "param": "param",
             "exception": "throws",
             "templateparam": "tparam",
-            "retval": "returns",
+            # retval support available on Sphinx >= 4.3
+            "retval": "returns" if sphinx.version_info[0:2] < (4, 3) else "retval",
         }
 
         # https://docutils.sourceforge.io/docs/ref/doctree.html#field-list
