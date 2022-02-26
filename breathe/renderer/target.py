098b68fd509f10df15a68013b3d2e41af3e3c603
2bndy5 <2bndy5@gmail.com>
2022-02-26 10:47:32 -0800

    revert an infered type (for mypy checking)

--- breathe/renderer/target.py
+++ breathe/renderer/target.py
@@ -3,11 +3,11 @@ from breathe.project import ProjectInfo
 from docutils import nodes
 from docutils.nodes import Element
 
-from typing import Any, Dict, List, Sequence
+from typing import Any, Dict, List
 
 
 class TargetHandler:
-    def create_target(self, refid: str) -> Sequence[Element]:
+    def create_target(self, refid: str) -> List[Element]:
         raise NotImplementedError
 
 
