4e9f55eadc91d96ac155a24c3634359b511a82cd
Michael Jones <m.pricejones@gmail.com>
2023-02-28 19:59:39 +0000

    version: release v4.35.0

--- breathe/__init__.py
+++ breathe/__init__.py
@@ -5,7 +5,7 @@ from breathe.renderer.sphinxrenderer import setup as renderer_setup
 from sphinx.application import Sphinx
 
 # Keep in sync with setup.py __version__
-__version__ = "4.34.0"
+__version__ = "4.35.0"
 
 
 def setup(app: Sphinx):
