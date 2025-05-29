3d703137d82ab08999204f93fb985594fd2363fb
Michael Jones <m.pricejones@gmail.com>
2025-05-29 11:07:55 +0100

    Resolve make.bat
    Keeping ruff logic over black & flake8.

--- make.bat
+++ make.bat
@@ -1,99 +1,3 @@
-<<<<<<< HEAD
-@ECHO OFF
-
-if "%1" == "html"           goto html
-if "%1" == "pdf"            goto pdf
-if "%1" == "data"           goto data
-if "%1" == "clean"          goto clean
-if "%1" == "distclean"      goto distclean
-if "%1" == "test"           goto test
-if "%1" == "dev-test"       goto dev-test
-if "%1" == "ruff"           goto ruff
-if "%1" == "type-check"     goto type-check
-if "%1" == "version-check"  goto version-check
-if "%1" == "all"            goto all
-goto end
-
-:html
-    call :data
-    cd documentation
-    call make.bat html
-    cd ..
-    goto end
-
-:pdf
-    call :data
-    cd documentation
-    call make.bat latexpdf
-    cd ..
-    goto end
-
-:data
-    cd examples\doxygen
-    call make.bat all
-    cd ..\..
-    cd examples\tinyxml
-    call make.bat all
-    cd ..\..
-    cd examples\specific
-    call make.bat all
-    cd ..\..
-    goto end
-
-:clean
-    cd examples\doxygen
-    call make.bat clean
-    cd ..\..
-    cd examples\tinyxml
-    call make.bat clean
-    cd ..\..
-    cd examples\specific
-    call make.bat clean
-    cd ..\..
-    goto end
-
-:distclean
-    call :clean
-    cd documentation
-    call make.bat clean
-    cd ..
-    goto end
-
-:test
-    cd tests
-    python -m pytest -v
-    cd ..
-    goto end
-
-:dev-test
-    cd tests
-    set PYTHONPATH=..\;%PYTHONPATH%
-    python -m pytest -v
-    cd ..
-    goto end
-
-:ruff
-    ruff check
-    ruff format
-    goto end
-
-:type-check
-    mypy
-    goto end
-
-:version-check
-    set PYTHONPATH=..\;%PYTHONPATH%
-    python scripts\version-check.py
-    goto end
-
-:all
-    call :html
-    call :pdf
-    goto end
-
-:end
-||||||| 542ae9b
-=======
 @ECHO OFF
 
 set GENERATED_MOD="breathe\_parser.py"
@@ -105,8 +9,7 @@ if "%1" == "clean"              goto clean
 if "%1" == "distclean"          goto distclean
 if "%1" == "test"               goto test
 if "%1" == "dev-test"           goto dev-test
-if "%1" == "flake8"             goto flake8
-if "%1" == "black"              goto black
+if "%1" == "ruff"               goto ruff
 if "%1" == "type-check"         goto type-check
 if "%1" == "version-check"      goto version-check
 if "%1" == "%GENERATED_MOD%"    goto _parser
@@ -176,12 +79,9 @@ goto end
     cd ..
     goto end
 
-:flake8
-    flake8 breathe
-    goto end
-
-:black
-    black --check .
+:ruff
+    ruff check
+    ruff format
     goto end
 
 :type-check
@@ -205,4 +105,3 @@ goto end
     goto end
 
 :end
->>>>>>> memberdef-in-groups
