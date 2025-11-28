e3099c55f178c0e1a11213c7eda04d8626d86c2b
Ronny Lorenz <ronny@tbi.univie.ac.at>
2025-11-28 12:25:12 +0100

    Add header file include lines as literal_block
    This commit adds a literal code block with an include statement
    for the respective symbol, e.g.
    
    	#include <bla.h>
    
    The code block will be placed between the brief and detailed description
    of the member definition.
    
    Whether or not the include statement will be produced can be controlled
    by the `breathe_show_decl_file_include` config setting (default: False)

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -224,6 +224,7 @@ def render(
     app.config.breathe_separate_member_pages = False
     app.config.breathe_use_project_refids = False
     app.config.breathe_show_define_initializer = show_define_initializer
+    app.config.breathe_show_decl_file_include = False
     app.config.breathe_order_parameters_first = False
     app.config.breathe_debug_trace_directives = False
     app.config.breathe_debug_trace_doxygen_ids = False
